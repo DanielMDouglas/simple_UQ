c43993fb780b50aaee96c1977f507ce6ea12658f
Daniel Miles Douglas <dougl215@sdfiana007.sdf.slac.stanford.edu>
2024-03-18 11:56:23 -0700

    SDF+PNN

--- loss.py
+++ loss.py
@@ -1,12 +1,46 @@
 import torch
 
-def MSELoss(output, truth):
-    return torch.mean(torch.pow(output - truth, 2))
+# def MSELoss(output, truth):
+#     return torch.mean(torch.pow(output - truth, 2))
 
-# def NLLLoss(mean, std, truth):
+def MSELoss(mean, var, truth):
+    return torch.mean(torch.pow(mean - truth, 2))
+
 def NLLLoss(mean, var, truth):
-    # logp = -0.5*torch.pow((mean - truth.T)/std, 2) - torch.log(std)
-    logp = -0.5*torch.pow(mean - truth.T, 2)/var - 0.5*torch.log(var)
+    # logp = -0.5*torch.pow(mean - truth.T, 2)/var - 0.5*torch.log(var)
+
+    # NLL = -torch.sum(logp)
+
+    # print ((torch.pow(mean- truth, 2)/var + torch.log(var)).shape)
+    # NLL = torch.sum(torch.pow(mean - truth, 2)/var + torch.log(var))
+    # NLL = torch.sum(torch.pow(mean - truth, 2))
+    NLL = torch.sum(0.5*torch.pow(mean - truth, 2)/var + 0.5*torch.log(var) + 0.5*torch.log(torch.tensor(2*torch.pi)))
     
-    # return torch.mean(torch.pow(mean - truth, 2))
-    return -torch.sum(logp)
+    return NLL
+
+def sampledMSE(mean, var, truth):
+    # logp = -0.5*torch.pow(mean - truth.T, 2)/var - 0.5*torch.log(var)
+
+    # NLL = -torch.sum(logp)
+
+    # print ((torch.pow(mean- truth, 2)/var + torch.log(var)).shape)
+    # NLL = torch.sum(torch.pow(mean - truth, 2)/var + torch.log(var))
+    # NLL = torch.sum(torch.pow(mean - truth, 2))
+    NLL = torch.sum(0.5*torch.pow(mean - truth, 2)/var + 0.5*torch.log(var) + 0.5*torch.log(torch.tensor(2*torch.pi)))
+
+    Nsamples = 20
+    # dist = torch.distributions.normal.Normal(loc = mean, scale = torch.sqrt(var))
+    dist = torch.distributions.normal.Normal(loc = torch.zeros_like(mean), scale = torch.ones_like(var))
+    # dist = torch.distributions.normal.Normal(loc = mean, scale = torch.ones_like(var))
+    # torch.mean(
+    sampleMags = dist.sample(torch.tensor([Nsamples]))
+    samples = mean + sampleMags*torch.sqrt(var)
+    # print ("inputs", mean, var)
+    # print ("samples", samples)
+    # print ("diff", samples - truth)
+    MSE = torch.mean(torch.pow(samples - truth, 2))
+    # print ("MSE", MSE)
+    
+    # return NLL
+
+    return MSE
--- network.py
+++ network.py
@@ -14,149 +14,178 @@ from loss import *
 
 from tqdm import tqdm
 
-Npoints = 10
-# Npoints = 3
-model = nn.Sequential(nn.Linear(in_features=2*Npoints, out_features=128),
-                      nn.ReLU(),
-                      nn.Linear(in_features=128, out_features=128),
-                      nn.ReLU(),
-                      nn.Linear(in_features=128, out_features=128),
-                      nn.ReLU(),
-                      nn.Linear(in_features=128, out_features=2)).to(device)
+import yaml
+import os
 
-# model = nn.Sequential(nn.Linear(in_features=2*Npoints, out_features=2, bias=False),
-#                       ).to(device)
+def loadManifestDict(manifest):
+    """
+    Load the manifest dictionary from a dictionary or a yaml path
+    """
+    if type(manifest) == dict: # if arg is a dict, do nothing
+        manifestDict = manifest
+    elif type(manifest) == str: # if arg is a yaml path, load
+        with open(manifest, 'r') as mf:
+            manifestDict = yaml.load(mf, Loader = yaml.FullLoader)
+            
+    assert type(manifestDict) == dict
 
-model.train()
+    return manifestDict
 
-# model[0].weight = nn.Parameter(torch.Tensor([[1, 1, 1, 0, 0, 0], 
-#                                              [0, 0, 0, 1, 1, 1]]).to(device))
-# print(model[0].weight.shape)
 
-pointDist = st.uniform(50, 100)
-noiseDist = st.uniform(0, 50)
-# noiseDist = st.uniform(0, 20)
+def main(args):
+    # manifestPath = 'singleLayer_uq.yaml'
+    # manifestPath = 'singleLayer_blind.yaml'
+    manifestPath = args.manifest
+    manifest = loadManifestDict(manifestPath)
 
-optimizer = optim.Adam(model.parameters(),
-                       lr = 1.e-2,
-                       # weight_decay = 1.,
-                   )
-# maxIter = 10000000
-maxIter = 10000
-# maxIter = 20000
-# maxIter = 4000
-# maxIter = 5
-# bs = 4
-# bs = 64
-# bs = 1024
-bs = 256
+    Npoints = manifest['Npoints']
 
-pbar = tqdm(range(maxIter))
+    if manifest['model'] == 'single_layer':
+        model = nn.Sequential(nn.Linear(in_features=2*Npoints, out_features=2, bias=False)).to(device)
+    elif manifest['model'] == '4_layer':
+        model = nn.Sequential(nn.Linear(in_features=2*Npoints, out_features=128),
+                              nn.ReLU(),
+                              nn.Linear(in_features=128, out_features=128),
+                              nn.ReLU(),
+                              nn.Linear(in_features=128, out_features=128),
+                              nn.ReLU(),
+                              nn.Linear(in_features=128, out_features=2)).to(device)
 
-load = False
-save = True
-verbose = False
+    model.train()
 
-# uq = True
-uq = False
+    if 'init_params' in manifest and manifest['init_params'] == True:
+        model[0].weight = nn.Parameter(torch.stack((torch.cat((torch.ones(Npoints),
+                                                               torch.zeros(Npoints))),
+                                                    torch.cat((torch.zeros(Npoints),
+                                                               torch.ones(Npoints))))).to(device))
 
-# weightFileName = 'NLLweights.ckpt'
-# weightFileName = 'NLLweights_7layer.ckpt'
+    refModel = nn.Sequential(nn.Linear(in_features=2*Npoints, out_features=2, bias=False)).to(device)
+    refModel.eval()
+    refModel[0].weight = nn.Parameter(torch.stack((torch.cat((torch.ones(Npoints),
+                                                              torch.zeros(Npoints))),
+                                                   torch.cat((torch.zeros(Npoints),
+                                                              torch.ones(Npoints))))).to(device))
 
-if uq:
-    # weightFileName = 'MLP_singleLayer_uq.ckpt'
-    # trainFileName = 'MLP_singleLayer_uq_train'
-
-    weightFileName = 'MLP_4layer_uq.ckpt'
-    trainFileName = 'MLP_4layer_uq_train'
-else:
-    # weightFileName = 'MLP_singleLayer_zeroblind.ckpt'
-    # trainFileName = 'MLP_singleLayer_zeroblind_train'
-
-    weightFileName = 'MLP_4layer_zeroblind.ckpt'
-    trainFileName = 'MLP_4layer_zeroblind_train'
-    
-# weightFileName = 'weights.ckpt'
-
-if load:
-    with open(weightFileName, 'rb') as f:
-        checkpoint = torch.load(f,
-                                map_location = device)
-        model.load_state_dict(checkpoint['model'], strict=False)
-
-trainList = []
-
-for iter in pbar:
-    inpt, inptNoise, truth = generate_input_batch(bs = bs,
-                                                  N = Npoints,
-                                                  points = pointDist,
-                                                  noise = noiseDist,
-                                                  label = sum)
-
-    optimizer.zero_grad()
-    # print (inpt.shape)
-    # print (inpt, inptNoise)
-    # print (torch.cat((inpt, inptNoise), dim = 1))
-    # print (torch.cat((inpt, inptNoise), dim = 1).shape)
-    if iter%100 == 0:
-        print(model[0].weight)
-
-    if uq:
-        output = model(torch.cat((inpt, inptNoise), dim = 1))
+    if 'point_params' in manifest:
+        pointParams = manifest['point_params']
     else:
-        output = model(torch.cat((inpt, torch.zeros_like(inpt)), dim = 1))
+        pointParams = (50, 100)
+    print ("point params", pointParams)
+    pointDist = st.uniform(*pointParams)
 
-    m_e = torch.sum(inpt, axis = 1)
-    # s_e = torch.sqrt(torch.sum(torch.pow(inptNoise, 2), axis = 1))
-    s_e = torch.sum(inptNoise, axis = 1)
-    # print (m_e.shape, s_e.shape)
+    if 'noise_params' in manifest:
+        noiseParams = manifest['noise_params']
+    else:
+        noiseParams = (0, 50)
+    print ("noise params", noiseParams)
+    noiseDist = st.uniform(*noiseParams)
 
-    mean = output[:,0]
-    # std = torch.abs(output[:,1])
-    var = torch.abs(output[:,1])
+    optimizer = optim.Adam(model.parameters(),
+                           lr = 1.e-2)
 
-    # if iter < maxIter/2:
-    # criterion = MSELoss
-    # loss = criterion(mean, truth.T)
-    # refLoss = criterion(m_e, truth.T)
-    # else:
-    criterion = NLLLoss
-    loss = criterion(mean, var, truth.T)
-    refLoss = criterion(m_e, s_e, truth.T)
-    pbar.set_description("loss: "+str(round(loss.item(), 2)) + " LLR: " + str(round(loss.item()/refLoss.item(), 2)))
+    maxIter = manifest['maxIter']
+    bs = manifest['batchSize']
 
-    trainList.append(loss.item())
-    
+    # load = False
+    load = manifest['resume']
+    # save = True
+    save = manifest['save']
 
-    # print (mean.shape, std.shape, truth.shape)
-    # print (m_e.shape, s_e.shape, truth.shape)
+    if 'verbose' in manifest:
+        verbose = manifest['verbose']
+    else:
+        verbose = False
 
-    # print (mean, var, truth)
-    # print (m_e, s_e, torch.sqrt(s_e), truth)
+    uq = manifest['input'] == 'uq'
 
-    # print (model.state_dict())
+    baseDir = manifest['baseDir']
+    os.makedirs(baseDir, exist_ok = True)
+    weightFileName = os.path.join(baseDir, manifest['weightFileName'])
+    trainFileName = os.path.join(baseDir, manifest['trainFileName'])
 
-    # print (criterion(m_e, s_e, truth))
+    if load:
+        with open(weightFileName, 'rb') as f:
+            checkpoint = torch.load(f,
+                                    map_location = device)
+            model.load_state_dict(checkpoint['model'], strict=False)
+
+    trainList = []
 
     if verbose:
-        print (inpt.shape)
-        print (truth.shape)
-    
-        print ("mean estimate", m_e)
-        print ("std estimate", s_e)
+        iterator = tqdm(range(maxIter))
+    else:
+        iterator = range(maxIter)
+            
+    for iter in iterator:
+        inpt, inptNoise, truth = generate_input_batch(bs = bs,
+                                                      N = Npoints,
+                                                      points = pointDist,
+                                                      noise = noiseDist,
+                                                      label = sum)
 
-        print ("mean inference", output[:,0])
-        print ("std inference", torch.abs(output[:,1]))
+        optimizer.zero_grad()
+
+        if save and iter%manifest['iterPerCKPT'] == 0:
+            torch.save(dict(model = model.state_dict()), weightFileName)
+            np.save(trainFileName, trainList)
         
-        print (truth.flatten())
-        print()
+        if uq:
+            output = model(torch.cat((inpt, inptNoise), dim = 1))
+        else:
+            output = model(torch.cat((inpt, torch.zeros_like(inpt)), dim = 1))
+
+        refOutput = refModel(torch.cat((inpt, inptNoise), dim = 1))
+        m_e = refOutput[:,0]
+        v_e = torch.abs(refOutput[:,1])
+        s_e = torch.sqrt(v_e)
+
+        mean = output[:,0]
+        var = torch.abs(output[:,1])
+        std = torch.sqrt(var)
+        
+        criterion = NLLLoss
+        # criterion = sampledMSE
+        loss = criterion(mean, var, truth.T)
+        refLoss = criterion(m_e, v_e, truth.T)
+
+        trainList.append(loss.item())
+        
+        if verbose:
+            iterator.set_description("loss: "+str(round(loss.item(), 2)) + " refLoss: "+str(round(refLoss.item(), 2)) + " LLR: " + str(round(loss.item()/refLoss.item(), 2)))
+
+            if iter%100 == 0:
+                print(model[0].weight)
+
+            # print (inpt.shape)
+            # print (truth.shape)
     
-    loss.backward()
-    optimizer.step()
+            # print ("mean estimate", m_e)
+            # print ("std estimate", v_e)
 
-print(model[0].weight)
+            # print ("mean inference", mean)
+            # print ("std inference", var)
+        
+            # print (truth.flatten())
+            # print()
+    
+        loss.backward()
+        optimizer.step()
 
-if save:
-    torch.save(dict(model = model.state_dict()), weightFileName)
-    np.save(trainFileName, trainList)
+    print("final weights:", model[0].weight)
 
+    if save:
+        torch.save(dict(model = model.state_dict()), weightFileName)
+        np.save(trainFileName, trainList)
+
+if __name__ == '__main__':
+    
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--manifest', type = str,
+                        default = "/sdf/home/d/dougl215/studies/NDLArSimReco/NDLArSimReco/manifests/testManifest.yaml",
+                        help = "network manifest yaml file")
+    
+    args = parser.parse_args()
+
+    main(args)
